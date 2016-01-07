32789de8786fe5e57fb50a635e25780ca6fd66ab
Valentyn Riabokonenko <Valentyn_Riabokonenko@epam.com>
2016-01-07 10:56:32 +0100

    boot example

---
+++ src/main/java/org/vr/config/TryoutConfiguration.java
@@ -0,0 +1,12 @@
+package org.vr.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryoutConfiguration {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TryoutConfiguration.class, args);
+    }
+}
